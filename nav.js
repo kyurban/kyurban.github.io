e6406a8ed88303a8a754727f07ccb5f9da560d75
Kyle Urban <155593997+kyurban@users.noreply.github.com>
2024-11-04 14:42:34 -0800

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,6 +1,16 @@
 const navbarToggle = document.getElementById('navbar-toggle');
 const nav = document.querySelector('nav');
+const navLinks = document.querySelectorAll('nav ul li a');
 
+// Toggle navigation visibility
 navbarToggle.addEventListener('click', () => {
     nav.classList.toggle('show-nav');
+});
+
+// Change color of clicked navigation link
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.forEach(navLink => navLink.classList.remove('active'));
+        link.classList.add('active');
+    });
 });
